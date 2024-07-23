eab842326ca420e9c20b537637283634a536032e
Tiza <tizadevel@tuta.io>
2024-07-23 10:20:42 -0600

    Add a <div> in the content section
    Add a div tag in the content section of the html file with a distinct size and background color.
    
    This serves as the laywork to get a grid of divs that will do the sketch. By now, just having a simple div that exists is enough.
    
    The div is created by the DOM methods of java script.

---
+++ script.js
@@ -0,0 +1,10 @@
+let base = "#191724";
+let love = "#eb6f92";
+const container = document.querySelector(".content");
+const div1 = document.createElement("div1");
+div1.style.backgroundColor = love;
+div1.style.height = "30px";
+div1.style.width = "30px";
+div1.style.borderRadius = "8px";
+
+container.appendChild(div1);
