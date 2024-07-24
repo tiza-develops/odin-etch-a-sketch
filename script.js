b4a0ac90847f2bbe694db4c91fbf94d1afeef930
Tiza <tizadevel@tuta.io>
2024-07-24 09:40:53 -0600

    Put boxes per row
    In this commit I manage to iterate over each row and insert multiple boxes. However, when I have to insert more than one box, it consumes way too many resources. So I should comprise both function into one alone.

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ function makeRow() {
   div1.className = "grid-row";
   div1.style.display = "flex";
   div1.style.flexDirection = "row";
+  div1.style.justifyContent= "center";
+  div1.style.alignItems= "center";
   div1.style.backgroundColor = pine;
   div1.style.height = "30px";
   div1.style.width = "80%";
   div1.style.margin = "2px 80px";
   div1.style.borderRadius = "8px";
-  container.appendChild(div1)
+  container.appendChild(div1);
+}
+function makeBox() {
+const rowContainer = document.querySelectorAll(".grid-row");
+rowContainer.forEach((element) => {
+  const div2 = document.createElement("div");
+  div2.className = "grid-row";
+  div2.style.display = "flex";
+  div2.style.flexDirection = "row";
+  div2.style.backgroundColor = love;
+  div2.style.height = "30px";
+  div2.style.width = "30px";
+  div2.style.margin = "2px";
+  div2.style.borderRadius = "8px";
+  element.appendChild(div2);
+  });
 }
 
 let i = 0;
@@ -22,20 +39,5 @@ while (i < 16) {
   i++;
 }
 
-function makeBox() {
-  const container = document.querySelector(".grid-row");
-  const div2 = document.createElement("div");
-  div2.className = "grid-box";
-  div2.style.backgroundColor = love;
-  div2.style.height = "30px";
-  div2.style.width = "30px";
-  div2.style.margin = "2px";
-  div2.style.borderRadius = "8px";
-  container.appendChild(div2)
-}
-
-let j = 0;
-while (j < 16) {
-  makeBox();
-  j++;
-}
+makeBox();
+makeBox();
