f6f75dac9f7c9c58579351422247f462142797b1
Tiza <tizadevel@tuta.io>
2024-07-24 07:51:42 -0600

    Display multiple divs that serve as rows
    In this commit I added lines to the script.js file that display various horizontal divs that (I hope) will serve as the rows of the grid of divs

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let base = "#191724";
 let love = "#eb6f92";
-const container = document.querySelector(".content");
-const div1 = document.createElement("div1");
-div1.style.backgroundColor = love;
-div1.style.height = "30px";
-div1.style.width = "30px";
-div1.style.borderRadius = "8px";
+let pine = "#31748f";
 
-container.appendChild(div1);
+function makeGrid() {
+  const container = document.querySelector(".content");
+  const div1 = document.createElement("div1");
+  div1.style.backgroundColor = pine;
+  div1.style.height = "30px";
+  div1.style.width = "80%";
+  div1.style.margin = "2px 80px";
+  div1.style.borderRadius = "8px";
+  container.appendChild(div1)
+}
+
+let i = 0;
+while (i < 16) {
+  makeGrid();
+  i++;
+}
