e340135ef8aad0bc44603454c6c586e990e52c3d
Tiza <tizadevel@tuta.io>
2024-07-24 08:01:13 -0600

    Add the boxes to one row
    This commit manages to (finally!) add the desired amount of boxes to one particular row. However, for reasons I do not know, it only does so in the first row.

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let base = "#191724";
 let love = "#eb6f92";
 let pine = "#31748f";
 
-function makeGrid() {
+function makeRow() {
   const container = document.querySelector(".content");
-  const div1 = document.createElement("div1");
+  const div1 = document.createElement("div");
+  div1.className = "grid-row";
+  div1.style.display = "flex";
+  div1.style.flexDirection = "row";
   div1.style.backgroundColor = pine;
   div1.style.height = "30px";
   div1.style.width = "80%";
@@ -15,6 +18,24 @@ function makeGrid() {
 
 let i = 0;
 while (i < 16) {
-  makeGrid();
+  makeRow();
   i++;
+}
+
+function makeBox() {
+  const container = document.querySelector(".grid-row");
+  const div2 = document.createElement("div");
+  div2.className = "grid-box";
+  div2.style.backgroundColor = love;
+  div2.style.height = "30px";
+  div2.style.width = "30px";
+  div2.style.margin = "2px";
+  div2.style.borderRadius = "8px";
+  container.appendChild(div2)
+}
+
+let j = 0;
+while (j < 16) {
+  makeBox();
+  j++;
 }
