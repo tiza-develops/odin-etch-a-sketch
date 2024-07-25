a33745b80bd69d1912d1b39fae825150421d5038
Tiza <tizadevel@tuta.io>
2024-07-24 19:38:03 -0600

    Square grid
    In this commit I finally managed to create a square-shaped grid. Ready to be merged into main branch

--- script.js
+++ script.js
@@ -2,42 +2,25 @@ let base = "#191724";
 let love = "#eb6f92";
 let pine = "#31748f";
 
-function makeRow() {
-  const container = document.querySelector(".content");
-  const div1 = document.createElement("div");
-  div1.className = "grid-row";
-  div1.style.display = "flex";
-  div1.style.flexDirection = "row";
-  div1.style.justifyContent= "center";
-  div1.style.alignItems= "center";
-  div1.style.backgroundColor = pine;
-  div1.style.height = "30px";
-  div1.style.width = "80%";
-  div1.style.margin = "2px 80px";
-  div1.style.borderRadius = "8px";
-  container.appendChild(div1);
-}
-function makeBox() {
-const rowContainer = document.querySelectorAll(".grid-row");
-rowContainer.forEach((element) => {
-  const div2 = document.createElement("div");
-  div2.className = "grid-row";
-  div2.style.display = "flex";
-  div2.style.flexDirection = "row";
-  div2.style.backgroundColor = love;
-  div2.style.height = "30px";
-  div2.style.width = "30px";
-  div2.style.margin = "2px";
-  div2.style.borderRadius = "8px";
-  element.appendChild(div2);
-  });
+const container = document.querySelector(".content");
+
+function makeGrid() {
+  for (i = 0; i < 16; i++) {
+      const row = document.createElement("div");
+      row.className = "row";
+      row.style.display = "flex";
+      container.appendChild(row);
+
+      for (j = 0; j < 16; j++) {
+        const col = document.createElement("div");
+        col.className = "col";
+        col.style.height = "30px";
+        col.style.width = "30px";
+        col.style.backgroundColor = love;
+        row.appendChild(col);
+        
+      }
+    }
 }
 
-let i = 0;
-while (i < 16) {
-  makeRow();
-  i++;
-}
-
-makeBox();
-makeBox();
+makeGrid();
